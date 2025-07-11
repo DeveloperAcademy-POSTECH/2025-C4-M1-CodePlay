3142ffe16f294c701182af70019ba72dc7403dea
yanni <iumd6075@gmail.com>
2025-07-12 00:19:53 +0900

    feat: 텍스트 인식 및 추출 viewModel 코드 작성

--- Projects/CodePlay/CodePlay/Presentation/Scene/MainView/ViewModel/MainViewModel.swift
+++ Projects/CodePlay/CodePlay/Presentation/Scene/MainView/ViewModel/MainViewModel.swift
@@ -6,22 +6,49 @@
 //
 
 import Foundation
+import UIKit
+internal import Combine
 
 // MARK: MainViewModelInput
 protocol MainViewModelInput {
-    
+    /// 페스티벌 이미지를 인식하는 함수
+    func recongizeFestivalLineup(from images: [UIImage])
+    /// 텍스트 초기화 함수
+    func clearText()
 }
 
 // MARK: MainViewModelOutput
 protocol MainViewModelOutput {
-    
+    var rawText: RawText? { get }
 }
 
 // MARK: MainViewModel
 protocol MainViewModel: MainViewModelInput, MainViewModelOutput { }
 
 // MARK: DefaultMainViewModel
-final class DefaultMainViewModel: MainViewModel {
+final class DefaultMainViewModel: MainViewModel, ObservableObject {
     
+    @Published var text = ""
+    var rawText: RawText?
+    private let recognizeTextUseCase: RecognizeTextUseCase
+    
+    init(text: String = "", rawText: RawText? = nil, recognizeTextUseCase: RecognizeTextUseCase) {
+        self.text = text
+        self.rawText = rawText
+        self.recognizeTextUseCase = recognizeTextUseCase
+    }
+    
+    func recongizeFestivalLineup(from images: [UIImage]) {
+        Task {
+            let result = try await recognizeTextUseCase.execute(with: images)
+            await MainActor.run {
+                self.rawText = result
+            }
+        }
+    }
+    
+    func clearText() {
+        rawText = nil
+    }
 }
 
