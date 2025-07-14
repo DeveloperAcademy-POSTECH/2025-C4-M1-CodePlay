5d66530df64a62272f57838a0d5dec7dfe22a4e4
dangdang1ing15 <shine_0512@naver.com>
2025-07-14 22:02:13 +0900

    Fix: 스캐너에서 모델로 데이터 저장 안되는 오류 해결

--- Projects/CodePlay/CodePlay/Presentation/Scene/MainView/View/ScannerView.swift
+++ Projects/CodePlay/CodePlay/Presentation/Scene/MainView/View/ScannerView.swift
@@ -6,77 +6,96 @@
 //
 
 import SwiftUI
-import VisionKit
 import Vision
+import VisionKit
 
 struct ScannerView: UIViewControllerRepresentable {
     @Environment(\.presentationMode) var presentationMode
     @Binding var recognizedText: String
-    
+
     var onComplete: ((RawText) -> Void)? = nil
-    
+
     func makeCoordinator() -> Coordinator {
         return Coordinator(self)
     }
-    
+
     func makeUIViewController(context: Context) -> some UIViewController {
         let scannerViewController = VNDocumentCameraViewController()
         scannerViewController.delegate = context.coordinator
         return scannerViewController
     }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
-    
+
+    func updateUIViewController(
+        _ uiViewController: UIViewControllerType,
+        context: Context
+    ) {}
+
     class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
         var parent: ScannerView
         var fullText: String = ""
-        
+
         init(_ parent: ScannerView) {
             self.parent = parent
         }
-        
-        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+
+        func documentCameraViewController(
+            _ controller: VNDocumentCameraViewController,
+            didFinishWith scan: VNDocumentCameraScan
+        ) {
             let dispatchGroup = DispatchGroup()
-            
+
             for pageIndex in 0..<scan.pageCount {
                 let image = scan.imageOfPage(at: pageIndex)
                 recognizeText(in: image)
             }
-            
+
             dispatchGroup.notify(queue: .main) {
-                            controller.dismiss(animated: true)
-                            self.parent.onComplete?(RawText(text: self.fullText))
+                controller.dismiss(animated: true)
+                self.parent.onComplete?(RawText(text: self.fullText))
             }
         }
-        
-        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+
+        func documentCameraViewControllerDidCancel(
+            _ controller: VNDocumentCameraViewController
+        ) {
             parent.presentationMode.wrappedValue.dismiss()
         }
-        
-        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+
+        func documentCameraViewController(
+            _ controller: VNDocumentCameraViewController,
+            didFailWithError error: Error
+        ) {
             print("Document scanning failed: \(error.localizedDescription)")
         }
-        
+
         private func recognizeText(in image: UIImage) {
             guard let cgImage = image.cgImage else { return }
-            
+
             let request = VNRecognizeTextRequest { (request, error) in
-                guard let observations = request.results as? [VNRecognizedTextObservation],
-                      error == nil else { return }
-                
+                guard
+                    let observations = request.results
+                        as? [VNRecognizedTextObservation],
+                    error == nil
+                else { return }
+
                 let recognizedStrings = observations.compactMap { observation in
                     observation.topCandidates(1).first?.string
                 }
-                
+
                 DispatchQueue.main.async {
-                    self.parent.recognizedText += recognizedStrings.joined(separator: "\n")
+                    let result = recognizedStrings.joined(separator: "\n")
+                    self.parent.recognizedText += result + "\n"
+                    self.fullText += result + "\n"
                 }
             }
-            
+
             request.recognitionLanguages = ["ko"]
             request.usesLanguageCorrection = true
 
-            let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            let requestHandler = VNImageRequestHandler(
+                cgImage: cgImage,
+                options: [:]
+            )
             do {
                 try requestHandler.perform([request])
             } catch {
