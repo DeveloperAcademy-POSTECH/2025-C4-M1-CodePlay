d7bfcf5ffa0829a0994765dc042b4441acef733d
yanni13 <iumd6075@gmail.com>
2025-07-26 00:59:45 +0900

    feat: 텍스트 필드 입력시 일치하는 검색 결과 리스트 UI 구현

--- Projects/CodePlay/CodePlay/Presentation/Scene/ExportPlaylist/View/FestivalCheck/FestivalSearchView.swift
+++ Projects/CodePlay/CodePlay/Presentation/Scene/ExportPlaylist/View/FestivalCheck/FestivalSearchView.swift
@@ -28,10 +28,8 @@ struct FestivalSearchView: View {
     // 검색 결과 더미 데이터
     let searchResults = [
         "2025 부산국제록페스티벌",
-        "2025 부산국제록페스티벌",
-        "2025 부산국제록페스티벌",
-        "2025 부산국제록페스티벌",
-        "2025 부산국제록페스티벌",
+        "2025 펜츠락페스티벌",
+        "22회 자라섬재즈페스티벌"
     ]
 
     var body: some View {
@@ -40,57 +38,11 @@ struct FestivalSearchView: View {
                 .backgroundWithBlur()
                 .ignoresSafeArea()
 
-            VStack(alignment: .leading) {
-
-                Spacer().frame(height: 26)
-
-                VStack(alignment: .leading, spacing: 20) {
-                    HStack {
-                        Text("추천 검색어")
-                            .foregroundColor(Color.neutral900)
-                            .font(.BmdBold())
-
-                        Spacer()
-
-                        Button(
-                            action: {
-                            },
-                            label: {
-                                Text("전체 삭제")
-                                    .font(.BmdRegular())
-                                    .foregroundColor(Color.neutral700)
-                            }
-                        )
-                    }
-                    LazyVGrid(
-                        columns: [
-                            GridItem(
-                                .adaptive(minimum: 150),
-                                alignment: .leading
-                            )
-
-                        ],
-                        spacing: 12
-                    ) {
-                        ForEach(recommendedSearches, id: \.self) { searchTerm in
-                            FestivalBox(title: searchTerm)
-                                .onTapGesture {
-                                    searchText = searchTerm
-                                    performSearch()
-                                }
-                        }
-                    }
-                    .frame(alignment: .leading)
-                }
-                .padding(.horizontal, 16)
-                .padding(.vertical, 24)
-                .frame(alignment: .topLeading)
-                .background(.white.opacity(0.6))
-                .cornerRadius(20)
-
-                Spacer()
+            if showSearchResults {
+                searchResultView
+            } else {
+                searchBeforeView
             }
-            .padding(.horizontal, 18)
         }
         .safeAreaInset(edge: .top) {
             Divider()
@@ -99,19 +51,39 @@ struct FestivalSearchView: View {
         .toolbar {
             // TODO: 특정뷰로 이동하도록 로직 수정 필요
             ToolbarItem(placement: .navigationBarLeading) {
-                Image(systemName: "chevron.left")
-                    .foregroundColor(.blue)
+                Button(
+                    action: {
+                        if showSearchResults {
+                            withAnimation(.easeInOut(duration: 0.3)) {
+                                showSearchResults = false
+                                searchText = ""
+                            }
+                        } else {
+                            dismiss()
+                        }
+                    },
+                    label: {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.blue)
+                    }
+                )
             }
 
             ToolbarItem(placement: .principal) {
                 SearchTextField(
                     text: $searchText,
-                    isSearchFocused: $isSearchFocused
+                    isSearchFocused: $isSearchFocused,
+                    onSearchSubmit: performSearch
                 )
             }
 
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button("취소") {
+                    if showSearchResults {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            showSearchResults = false
+                        }
+                    }
                     searchText = ""
                 }
             }
@@ -123,6 +95,93 @@ struct FestivalSearchView: View {
         }
     }
 
+    // MARK: searchResultsView
+    @ViewBuilder
+    private var searchBeforeView: some View {
+        VStack(alignment: .leading) {
+
+            Spacer().frame(height: 26)
+
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text("추천 검색어")
+                        .foregroundColor(Color.neutral900)
+                        .font(.BmdBold())
+
+                    Spacer()
+
+                    Button(
+                        action: {
+                        },
+                        label: {
+                            Text("전체 삭제")
+                                .font(.BmdRegular())
+                                .foregroundColor(Color.neutral700)
+                        }
+                    )
+                }
+                LazyVGrid(
+                    columns: [
+                        GridItem(
+                            .adaptive(minimum: 150),
+                            alignment: .leading
+                        )
+
+                    ],
+                    spacing: 12
+                ) {
+                    ForEach(recommendedSearches, id: \.self) { searchTerm in
+                        FestivalBox(title: searchTerm)
+                            .onTapGesture {
+                                searchText = searchTerm
+                                performSearch()
+                            }
+                    }
+                }
+                .frame(alignment: .leading)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 24)
+            .frame(alignment: .topLeading)
+            .background(.white.opacity(0.6))
+            .cornerRadius(20)
+
+            Spacer()
+        }
+        .padding(.horizontal, 18)
+    }
+
+    @ViewBuilder
+    private var searchResultView: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Spacer().frame(height: 26)
+
+            LazyVStack(spacing: 12) {
+                ForEach(searchResults, id: \.self) { result in
+                    HStack {
+                        Text(result)
+                            .font(.BlgBold())
+                            .foregroundColor(Color.neutral700)
+                            .lineSpacing(2)
+                            .padding(.leading, 12)
+                            .padding(.vertical, 13)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 2)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(.white.opacity(0.9), lineWidth: 2)
+                    )
+                    .background(.white.opacity(0.3))
+                    .cornerRadius(12)
+                }
+            }
+            .padding(.horizontal, 18)
+
+            Spacer()
+        }
+    }
     /// 검색어와 일치하는 결과가 있는지 확인하는 함수
     private func performSearch() {
         if !searchText.isEmpty {
@@ -143,6 +202,7 @@ struct FestivalSearchView: View {
 struct SearchTextField: View {
     @Binding var text: String
     @FocusState.Binding var isSearchFocused: Bool
+    let onSearchSubmit: () -> Void
 
     var body: some View {
         HStack {
@@ -150,6 +210,9 @@ struct SearchTextField: View {
                 .focused($isSearchFocused)
                 .textFieldStyle(PlainTextFieldStyle())
                 .font(.BmdRegular())
+                .onSubmit {
+                    onSearchSubmit()
+                }
 
             if !text.isEmpty {
                 Button(action: {
