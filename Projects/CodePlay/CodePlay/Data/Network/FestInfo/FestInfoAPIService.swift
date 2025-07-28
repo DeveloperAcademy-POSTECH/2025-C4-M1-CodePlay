f870e743bb3da4ba46d2abde04ee956f5affcb18
dangdang1ing15 <shine_0512@naver.com>
2025-07-28 09:58:18 +0900

    Fix: 중복DTO 문제 해결

--- Projects/CodePlay/CodePlay/Data/Network/FestInfo/FestInfoAPIService.swift
+++ Projects/CodePlay/CodePlay/Data/Network/FestInfo/FestInfoAPIService.swift
@@ -11,7 +11,6 @@ import Moya
 // MARK: NotificationAPIService
 protocol FestInfoAPIServiceProtocol {
     func postFestInfoText(model: PostFestInfoTextRequestDTO) async throws -> PostFestInfoResponseDTO
-    func postFestInfoVision(model: PostFestInfoVisionRequestDTO) async throws -> PostFestInfoResponseDTO
 }
 
 // MARK: DefaultNotificationAPIService
@@ -21,23 +20,6 @@ final class FestInfoAPIService: BaseAPIService<FestInfoTargetType>, FestInfoAPIS
     
     func postFestInfoText(model: PostFestInfoTextRequestDTO) async throws -> PostFestInfoResponseDTO {
         let response = try await provider.request(.postFestInfoText(model: model))
-        
-        let result: NetworkResult<PostFestInfoResponseDTO> = fetchNetworkResult(
-            statusCode: response.statusCode,
-            data: response.data
-        )
-        
-        switch result {
-        case .success(let data):
-            guard let data else { throw NetworkResult<Error>.decodeErr }
-            return data
-        default:
-            throw NetworkResult<Error>.networkFail
-        }
-    }
-    
-    func postFestInfoVision(model: PostFestInfoVisionRequestDTO) async throws -> PostFestInfoResponseDTO {
-        let response = try await provider.request(.postFestInfoVision(model: model))
         
         let result: NetworkResult<PostFestInfoResponseDTO> = fetchNetworkResult(
             statusCode: response.statusCode,
--- Projects/CodePlay/CodePlay/Data/Network/FestInfo/PostInfoTargetType.swift
+++ Projects/CodePlay/CodePlay/Data/Network/FestInfo/PostInfoTargetType.swift
@@ -11,7 +11,6 @@ internal import Alamofire
 
 enum FestInfoTargetType {
     case postFestInfoText(model: PostFestInfoTextRequestDTO)
-    case postFestInfoVision(model: PostFestInfoVisionRequestDTO)
 }
 
 extension FestInfoTargetType: BaseTargetType {
@@ -24,32 +23,24 @@ extension FestInfoTargetType: BaseTargetType {
         switch self {
         case .postFestInfoText:
             return ["Content-Type": "application/json"]
-            
-        case .postFestInfoVision:
-            return ["Content-Type": "application/json"]
         }
     }
     
     var path: String {
         switch self {
         case .postFestInfoText: return utilPath.rawValue + "/search"
-        case .postFestInfoVision: return utilPath.rawValue + "/search"
         }
     }
     
     var method: Moya.Method {
         switch self {
         case .postFestInfoText: return .post
-        case .postFestInfoVision: return .post
         }
     }
     
     var task: Task {
         switch self {
         case let .postFestInfoText(model):
-            return .requestJSONEncodable(model)
-            
-        case let .postFestInfoVision(model):
             return .requestJSONEncodable(model)
         }
     }
